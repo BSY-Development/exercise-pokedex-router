66de9fa427aa792aeef0d46c342342168f651474
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-07-05 20:17:00 -0300

    Feito o PokeInfos para que ele receba as divs contendo as informacoes do pokemon separadamente

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <h1> Pokedex </h1>
         <Switch>
-          <Route path="/pokemons/:id" component={PokeInfos} />
+          <Route path="/pokemons/:id" render={(props) => <PokeInfos {...props} pokemons={pokemons} />} />
           <Route path="/" render={(props) => <Pokedex {...props} pokemons={pokemons} />} />
         </Switch>
       </div>
--- src/components/PokeInfos.js
+++ src/components/PokeInfos.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import Pokemon from '../Pokemon';
 
 export default class PokeInfos extends React.Component {
   render() {
-    const { id } = this.props.match.params; 
+    const { id } = this.props.match.params;
+    const { pokemons } = this.props;
+    const pokemonSelected = pokemons.find((item) => item.id === parseInt(id, 10))
+    const { name, summary, foundAt } = pokemonSelected;
     return (
-      <h1>Infos {id}</h1>
+      <div>
+        <h1>{name} Details</h1>
+        <Pokemon pokemon={pokemonSelected} />
+        <h1>Summary</h1>
+        <p>{summary}</p>
+        <h1>Game Locations of {name}</h1>
+        <div className="map-style">
+          { foundAt.map((item, index) => {
+            return (
+              <div key={index}>
+                <img key={item.map} src={item.map} alt={name} />
+                <p key={item.location}>{item.location}</p>
+              </div>
+            );
+          }) }
+        </div>
+      </div>
     );
   }
 }
